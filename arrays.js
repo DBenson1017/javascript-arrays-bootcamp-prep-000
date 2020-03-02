5d051ca96a842a24e49c0b6057a8009dfd01248a
Devin Benson <devinbenson@gmail.com>
2020-03-02 23:03:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 function addElementToEndOfArray(array,element){
   array = []
-  array.push(element)
+  array.push(${element})
   return array
 }
 function destructivelyAddElementToEndOfArray(array,element){
